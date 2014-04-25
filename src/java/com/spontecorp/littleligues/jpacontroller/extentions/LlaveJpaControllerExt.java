229b6a6ea2dc4f1ed9bb615b21e4511afa2ef454
Zuleidyb <Zuleidyb@Zuleidy>
2014-04-25 12:20:30 -0400

    Errores Administración

--- src/java/com/spontecorp/littleligues/jpacontroller/extentions/LlaveJpaControllerExt.java
+++ src/java/com/spontecorp/littleligues/jpacontroller/extentions/LlaveJpaControllerExt.java
@@ -24,6 +24,7 @@ public class LlaveJpaControllerExt extends LlaveJpaController{
     }
     
     public List<Llave> findLlavesOnFase(Fase fase){
+        //System.out.println("IdFase: " + fase.getId());
         EntityManager em = getEntityManager();
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Llave> cq = cb.createQuery(Llave.class);
@@ -58,7 +59,7 @@ public class LlaveJpaControllerExt extends LlaveJpaController{
             return llaves;
         }
     }
-
+    
     public Object findLlave(String value) {
         EntityManager em = getEntityManager();
         try {
@@ -68,6 +69,41 @@ public class LlaveJpaControllerExt extends LlaveJpaController{
             cq.select(llave);
             cq.where(cb.equal(llave.get(Llave_.nombre), value));
             TypedQuery<Llave> q = em.createQuery(cq);
+            //System.out.println("1.- Metodo findLlave. Result: " + q.getResultList().get(0) + " " + q.getResultList().get(0).getId());
+            return q.getResultList().get(0);
+        } finally {
+            em.close();
+        }
+    }
+
+    public Object findLlave(String value, Fase faseId) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Llave> cq = cb.createQuery(Llave.class);
+            Root<Llave> llave = cq.from(Llave.class);
+            cq.select(llave);
+            cq.where(cb.equal(llave.get(Llave_.nombre), value));
+            cq.where(cb.equal(llave.get(Llave_.faseId), faseId));
+            TypedQuery<Llave> q = em.createQuery(cq);
+            //System.out.println("1.- Metodo findLlave. Result: " + q.getResultList().get(0) + " " + q.getResultList().get(0).getId());
+            return q.getResultList().get(0);
+        } finally {
+            em.close();
+        }
+    }
+    
+    public Llave findLlaveFinal(String value, Fase fase) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Llave> cq = cb.createQuery(Llave.class);
+            Root<Llave> llave = cq.from(Llave.class);
+            cq.select(llave);
+            cq.where(cb.equal(llave.get(Llave_.nombre), value));
+            cq.where(cb.equal(llave.get(Llave_.faseId), fase));
+            TypedQuery<Llave> q = em.createQuery(cq);
+            System.out.println("2.- Metodo: findLlaveFinal. Result: " + q.getResultList().get(0) + " " + q.getResultList().get(0).getId());
             return q.getResultList().get(0);
         } finally {
             em.close();
